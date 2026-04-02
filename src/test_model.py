11bb71958a93c827597cdef8fb70eb765f1fcf13
Jérémie Crosnier <jeremiecrosnier@gmail.com>
2026-04-02 17:30:54 +0200

    amélioration de test_model

--- src/test_model.py
+++ src/test_model.py
@@ -1,8 +1,19 @@
 import cv2
+import os
 from ultralytics import YOLO
 
+IMAGE_EXTS = [".jpg", ".jpeg", ".png", ".bmp", ".webp", ".jfif"]
+DOSSIER = "./TEST"
+
 model = YOLO("../runs/detect/pfc_model2/weights/best.pt")
-results = model("./TEST/IMG_20260217_134055.jpg", save=True)
+#results = model("./TEST/IMG_20260217_134055.jpg", save=True)
+
+for file in os.listdir(DOSSIER):
+    for ext in IMAGE_EXTS:
+        if file.endswith(ext):
+            img_path = os.path.join(DOSSIER, file)
+            results = model(img_path, save=True)
+
 """annotated = results[0].plot()
 
 cv2.imshow("Result", annotated)
